f6c3f8a0982be92d9d6240db9bf39422fd42a93c
Maciej Palubski <palubskimaciej@gmail.com>
2023-10-01 15:17:52 +0200

    keyboard support added

--- js/script.js
+++ js/script.js
@@ -7,6 +7,24 @@ const arrowLeft = document.querySelector(".popup__arrow--left");
 
 let currentImgIndex;
 
+const showNextImage = () => {
+  if (currentImgIndex === galleryItems.length - 1) {
+    currentImgIndex = 0;
+  } else {
+    currentImgIndex = currentImgIndex + 1;
+  }
+  popupImage.src = galleryItems[currentImgIndex].src;
+};
+
+const showPreviousImage = () => {
+  if (currentImgIndex === 0) {
+    currentImgIndex = galleryItems.length - 1;
+  } else {
+    currentImgIndex = currentImgIndex - 1;
+  }
+  popupImage.src = galleryItems[currentImgIndex].src;
+};
+
 galleryItems.forEach((galleryItem, index) => {
   galleryItem.addEventListener("click", ({ target }) => {
     popup.classList.toggle("popup__hidden");
@@ -15,24 +33,23 @@ galleryItems.forEach((galleryItem, index) => {
   });
 });
 
-popupClose.addEventListener("click", () => {
+const galleryClose = () => {
   popup.classList.add("popup__hidden");
-});
+};
 
-arrowRight.addEventListener("click", () => {
-  if (currentImgIndex === galleryItems.length - 1) {
-    currentImgIndex = 0;
-  } else {
-    currentImgIndex = currentImgIndex + 1;
-  }
-  popupImage.src = galleryItems[currentImgIndex].src;
-});
+popupClose.addEventListener("click", galleryClose);
 
-arrowLeft.addEventListener("click", () => {
-  if (currentImgIndex === 0) {
-    currentImgIndex = galleryItems.length - 1;
-  } else {
-    currentImgIndex = currentImgIndex - 1;
+arrowRight.addEventListener("click", showNextImage);
+
+arrowLeft.addEventListener("click", showPreviousImage);
+
+document.addEventListener("keydown", (event) => {
+  console.log(event);
+  if (event.code === "ArrowRight" || event.keyCode === 39) {
+    showNextImage();
+  } else if (event.code === "ArrowLeft" || event.keyCode === 37) {
+    showPreviousImage();
+  } else if (event.code === "Escape" || event.keyCode === 27) {
+    galleryClose();
   }
-  popupImage.src = galleryItems[currentImgIndex].src;
 });
